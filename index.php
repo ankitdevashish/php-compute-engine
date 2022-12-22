950cbbd5e2de431a47d097f39f239a9e3612a0cb
Devashish <ankitdevashish@gmail.com>
2022-12-22 23:05:05 +0530

    test

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
 <?php
-    echo "testint compute engine, from cloud build, iu";  
+require 'vendor/autoload.php';
+
+use Google\Cloud\Storage\StorageClient;
+use Google\Cloud\SecretManager\V1\Replication;
+use Google\Cloud\SecretManager\V1\Replication\Automatic;
+use Google\Cloud\SecretManager\V1\Secret;
+use Google\Cloud\SecretManager\V1\SecretManagerServiceClient;
+
+// Authenticating with keyfile data.
+$storage = new StorageClient([
+    'keyFile' => json_decode(file_get_contents('./avian-slice-371209-3dd89dbc794b.json'), true)
+]);
+
+// Authenticating with a keyfile path.
+$storage = new StorageClient([
+    'keyFilePath' => './avian-slice-371209-3dd89dbc794b.json'
+]);
+
+// Providing the Google Cloud project ID.
+$storage = new StorageClient([
+    'projectId' => 'myProject'
+]);
+
+$client = new SecretManagerServiceClient();
+
+$secret = $client->createSecret(
+    SecretManagerServiceClient::projectName('avian-slice-371209'),
+    'name2',
+    new Secret([
+        'replication' => new Replication([
+            'automatic' => new Automatic()
+        ])
+    ])
+);
+
+    echo "testint compute engine, from cloud build, iu". $secret->getName();  
+
+    
+printf(
+    'Created secret: %s' . PHP_EOL,
+    $secret->getName()
+);
+
 ?>
